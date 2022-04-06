446ee1ba09a344fe24b5034a9a578ba50093a0cd
Sorrond <rafaelsorrond@gmail.com>
2022-04-06 10:44:49 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var roomsRouter = require('./routes/roomsRoutes');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/rooms', roomsRouter);
+
+module.exports = app;
+
