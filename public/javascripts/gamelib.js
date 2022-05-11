420a2b9da5901089318da8b6bfa6bfa7c3be3367
Mobylia <atomicbread@outlook.com>
2022-05-11 14:08:08 +0100

    Dice Rework

--- public/javascripts/gamelib.js
+++ public/javascripts/gamelib.js
@@ -96,55 +96,17 @@ let roll2=2;
 let result=0;
 
 //Dice 
-function dice(diceX, diceY,roll) {
-
-  if (roll==1) {
-    fill(255, 255, 255)
-    rect(diceX, diceY, 50, 50, 10)
-    fill(0, 0, 0)
-    text("1", diceX - 4, diceY + 5)
-  }
-  if (roll==2) {
-    fill(255, 255, 255)
-    rect(diceX, diceY, 50, 50, 10)
-    fill(0, 0, 0)
-    text("2", diceX - 4, diceY + 5)
-  }
-  if (roll==3) {
-    fill(255, 255, 255)
-    rect(diceX, diceY, 50, 50, 10)
-    fill(0, 0, 0)
-    text("3", diceX - 4, diceY + 5)
-  }
-  if (roll==4) {
-    fill(255, 255, 255)
-    rect(diceX, diceY, 50, 50, 10)
-    fill(0, 0, 0)
-    text("4", diceX - 4, diceY + 5)
-  }
-  if (roll==5) {
-    fill(255, 255, 255)
-    rect(diceX, diceY, 50, 50, 10)
-    fill(0, 0, 0)
-    text("5", diceX - 4, diceY + 5)
-  }
-  if (roll==6) {
-    fill(255, 255, 255)
-    rect(diceX, diceY, 50, 50, 10)
-    fill(0, 0, 0)
-    text("6", diceX - 4, diceY + 5)
-  }  
-}
-//Dice roll 
-function mouseReleased()
-{
-  roll1=int(random(1,7))
-  roll2=int(random(1,7))
+function newTurn(){
+  if newTurn
+  then
+    roll1=int(random(1,7))
+    roll2=int(random(1,7))
 }
 
 //Result 
-function mouseClicked()
+function showResult()
 {
   result= roll1 + roll2
+  print("your points are:" + result)
 }
 */
