9cc407db33a2f8f433a60043c08dc4433469bff1
Sorrond <rafaelsorrond@gmail.com>
2022-05-23 10:56:36 +0100

    verification before moving a boat

--- models/boardModel.js
+++ models/boardModel.js
@@ -1,3 +1,4 @@
+const req = require('express/lib/request');
 var pool = require('./connection.js')
 
 module.exports.getBoard = async function () {
@@ -24,7 +25,7 @@ module.exports.getBoard = async function () {
 module.exports.getGameBits = async function (id) {
   try {
     let sql = "SELECT ($2 = roomuser_user_id) as is_player, objecttile_tile_i, objecttile_tile_j, objecttype_name, objecttile_object_current_health FROM objecttile INNER JOIN object_ ON objecttile_object_id = object_id INNER JOIN objecttype ON objecttype_id = object_type_id INNER JOIN roomuser ON roomuser_id = object_roomuser_id WHERE roomuser_room_id = $1";
-    let result = await pool.query(sql,[1,id]);
+    let result = await pool.query(sql, [1, id]);
     let gamebits = result.rows;
     return { status: 200, result: { gamebits } };
   } catch (err) {
@@ -47,9 +48,13 @@ module.exports.getGameBitsByTile = async function (tile_i, tile_j) {
 
 module.exports.moveBoatsById = async function (gamebit_id, tile_i, tile_j) {
   try {
-    let sql1 = "UPDATE objecttile SET objecttile_tile_i = $1, objecttile_tile_j = $2 WHERE objecttile_object_id = $3";
-    let result = await pool.query(sql1, [tile_i, tile_j, gamebit_id]);
-    return { status: 200, result: result };
+    let sql = "select (roomuser_user_id = (Select roomuser_id from roomuser WHERE roomuser_user_id = $1 AND roomuser_room_id = $2)) from roomuser INNER JOIN turn ON turn_roomuser_id = roomuser_id Where turn_id = (Select max (turn_id) from turn INNER JOIN roomuser on roomuser_id = turn_roomuser_id WHERE roomuser_room_id = $2)";
+    let result = await pool.query(sql, [req.userId, 1]);
+    if (result) {
+      let sql1 = "UPDATE objecttile SET objecttile_tile_i = $1, objecttile_tile_j = $2 WHERE objecttile_object_id = $3";
+      result = await pool.query(sql1, [tile_i, tile_j, gamebit_id]);
+      return { status: 200, result: result };
+    }
   } catch (err) {
     console.log(err);
     return { status: 500, result: err };
@@ -60,7 +65,7 @@ module.exports.damageObject = async function (object_id, damage_object_tile_i, d
   try {
     let sql = "UPDATE objecttile SET objecttile_object_current_health = false WHERE objecttile_tile_i = $1 AND objecttile_tile_j = $2 AND objecttile_object_id = $3";
     let result = await pool.query(sql, [damage_object_tile_i, damage_object_tile_j, object_id]);
-    return { status: 200, result: result};
+    return { status: 200, result: result };
   } catch (err) {
     console.log(err);
     return { status: 500, result: err };
@@ -84,7 +89,7 @@ module.exports.deleteBoat = async function (gamebit_id, gamebit_tile_i, gamebit_
     let result = await pool.query(sql, [gamebit_tile_i, gamebit_tile_j, gamebit_id]);
     let sql1 = "DELETE FROM object_ WHERE object_id = $1";
     result = await pool.query(sql1, [gamebit_id]);
-    return { status: 200, result: result};
+    return { status: 200, result: result };
   } catch (err) {
     console.log(err);
     return { status: 500, result: err };
